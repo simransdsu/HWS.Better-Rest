60a1b1a0e0d3e86b01679a8535b5bafdb58a75e0
Simran Preet Singh <simran5590@gmail.com>
2022-06-01 07:24:19 -0700

    Initial Commit

---
+++ Better Rest/Better_RestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Better_RestApp.swift
+//  Better Rest
+//
+//  Created by Simran Preet Narang on 2022-06-01.
+//
+
+import SwiftUI
+
+@main
+struct Better_RestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Better Rest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Better Rest
+//
+//  Created by Simran Preet Narang on 2022-06-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
